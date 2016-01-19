943fd084a0691a29223b89fce878923f3ff19d0d
Kim Hyojong <jpd4109@gmail.com>
2016-01-19 15:28:43 +0000

    added library to convert local DB file to JSON to sync with server or should i use syncadapters?

---
+++ app/src/main/java/com/example/justin/alarmclockv2/ConvertDBtoJson.java
@@ -0,0 +1,12 @@
+package com.example.justin.alarmclockv2;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+/**
+ * Created by Justin on 1/19/16.
+ */
+public class ConvertDBtoJson {
+
+    ArrayList infoList = (ArrayList)
+
+}
